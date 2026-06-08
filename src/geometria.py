0c63eef33a5f09159cc600ad19e40125a1171069
rgarvel709 <145475048+rgarvel709@users.noreply.github.com>
2026-06-08 15:01:25 +0200

    Add function to calculate rectangle area

---
+++ src/geometria.py
@@ -0,0 +1,9 @@
+def calcular_area(base, altura):
+    """
+    Calcula el área de un rectángulo.
+
+    :param base: La base del rectángulo.
+    :param altura: La altura del rectángulo.
+    :return: El área calculada.
+    """
+    return base * altura
